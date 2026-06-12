e6fbdc2f1e634e92d03c36d10740f236c0cb28e2
Teju Patel <tejupateltejupatel0@gmail.com>
2026-06-12 23:41:52 +0530

    added purchase request workflow and doctype

---
+++ college_management/college_management/doctype/purchase_request/test_purchase_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Teju and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPurchaseRequest(FrappeTestCase):
+	pass
--- college_management/hooks.py
+++ college_management/hooks.py
@@ -246,4 +246,10 @@ app_license = "mit"
 # ------------
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
-
+# fixtures = [
+#     "Workflow",
+#     "Role",
+#     "Custom Field",
+#     "Property Setter",
+#     "DocType"
+# ]
